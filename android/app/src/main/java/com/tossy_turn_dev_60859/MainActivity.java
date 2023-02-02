f2828de2e267711cf620871a443be59b8b4ece53
Crowdbotics <engineering@crowdbotics.com>
2023-02-02 23:12:08 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tossy_turn_dev_60859/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tossy_turn_dev_60859;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "tossy_turn_dev_60859";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tossy-turn-dev-60859",
+  url: "https://tossy-turn-dev-60859.botics.co",
+  api: "https://tossy-turn-dev-60859.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
